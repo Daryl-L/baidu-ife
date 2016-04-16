5e08aeac682658ad229c4635fe52cc863a2775cf
daryl <sai9375@hotmail.com>
2016-04-16 23:54:02 +0800

    任务十七完成20%

---
+++ task017/task017.js
@@ -0,0 +1,42 @@
+// 以下两个函数用于随机模拟生成测试数据
+function getDateStr(dat) {
+    var y = dat.getFullYear();
+    var m = dat.getMonth() + 1;
+    m = m < 10 ? '0' + m : m;
+    var d = dat.getDate();
+    d = d < 10 ? '0' + d : d;
+    return y + '-' + m + '-' + d;
+}
+
+function randomBuildData(seed) {
+    var returnData = {};
+    var dat = new Date("2016-01-01");
+    var datStr = ''
+    for (var i = 1; i < 92; i++) {
+        datStr = getDateStr(dat);
+        returnData[datStr] = Math.ceil(Math.random() * seed);
+        dat.setDate(dat.getDate() + 1);
+    }
+    return returnData;
+}
+
+var aqiSourceData = {
+    "北京": randomBuildData(500),
+    "上海": randomBuildData(300),
+    "广州": randomBuildData(200),
+    "深圳": randomBuildData(100),
+    "成都": randomBuildData(300),
+    "西安": randomBuildData(500),
+    "福州": randomBuildData(100),
+    "厦门": randomBuildData(100),
+    "沈阳": randomBuildData(500)
+};
+
+//用来渲染图标的数据
+var chartData = {};
+
+// 记录当前页面的表单选项
+var pageState = {
+    nowSelectCity: -1,
+    nowGraTime: "day"
+};
